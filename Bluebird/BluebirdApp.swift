0d72d31aca6bcdfc12d682c0c0b7f94d673b3310
FergusJJ <fergus2004johnson@gmail.com>
2025-04-18 10:26:17 +0100

    initial swift app

---
+++ Bluebird/BluebirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluebirdApp.swift
+//  Bluebird
+//
+//  Created by fergus johnson on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BluebirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluebirdTests/BluebirdTests.swift
@@ -0,0 +1,17 @@
+//
+//  BluebirdTests.swift
+//  BluebirdTests
+//
+//  Created by fergus johnson on 18/04/2025.
+//
+
+import Testing
+@testable import Bluebird
+
+struct BluebirdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
